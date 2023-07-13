e8bfd81f83a7c586040e8a457a5577a12d0857e9
sadbatya <sadbatya@mail.ru>
2023-07-13 17:40:02 +0200

    push

--- src/components/Profile/MyPosts/MyPost.jsx
+++ src/components/Profile/MyPosts/MyPost.jsx
@@ -3,11 +3,16 @@ import classes from './MyPost.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+  let posts = [
+    { id: 1, message: 'Hi. how are you?', likesCount: 12 },
+    { id: 2, message: 'It`s My first post', likesCount: 10 },
+    { id: 3, message: 'BlaBla', likesCount: 15 },
+    { id: 4, message: 'BlaBlaBLA', likesCount: 18 },
+  ];
 
-  let postsData = [
-    {id: 1, message: 'Hi. how are you?', likesCount: 12},
-    {id: 2, message: 'It`s My first post', likesCount: 10},
-  ]
+  let postsElements = posts.map((p) => (
+    <Post message={p.message} likeCount={p.likesCount} />
+  ));
   return (
     <div className={classes.postsBlock}>
       <h3>My posts</h3>
@@ -16,12 +21,9 @@ const MyPosts = () => {
           <textarea name='' id='' cols='30' rows='5'></textarea>
         </div>
         <button>add post</button>
+        <button>remove</button>
       </div>
-      <div className={classes.posts}>
-        <Post message={postsData[0].message} likeCount={postsData[0].likesCount} />
-        <Post message={postsData[1].message} likeCount={postsData[1].likesCount} />
-      </div>
-      <button>remove</button>
+      <div className={classes.posts}>{postsElements}</div>
     </div>
   );
 };
